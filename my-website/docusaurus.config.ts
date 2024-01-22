a77a5e1bbf0f15f658cd8f0a4e49f0b3bd0983f8
若汝棋茗 <505554090@qq.com>
2024-01-22 15:22:12 +0800

    2

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://euphonious-starship-4084d0.netlify.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/abc/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
